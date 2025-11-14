74ad6dbf018c29992625b775dc1ef99dd8faf5f7
hypnozee <razvan_92@yahoo.com>
2025-11-15 00:16:08 +0200

    Initial Commit

---
+++ PokeDexTests/PokeDexTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexTests.swift
+//  PokeDexTests
+//
+//  Created by Razvan Barbu on 15.11.2025.
+//
+
+import Testing
+@testable import PokeDex
+
+struct PokeDexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
